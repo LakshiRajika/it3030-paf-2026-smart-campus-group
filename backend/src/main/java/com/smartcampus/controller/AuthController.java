6c7973940c7bebb0891789675a27c6fc2bca6bae
HImaya_24 <himayapalliyaguruge@gmail.com>
2026-04-12 10:05:54 +0530

    changed the structure

---
+++ backend/src/main/java/com/smartcampus/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.smartcampus.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    // Add endpoints here
+}
